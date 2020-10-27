8b4db40bff1eab48e9fbd7ee9887638478551f3b
Julian Frosch <julian.frosch@gmail.com>
2020-10-27 12:06:37 +0100

    experimental wip

--- src/js/fullscreen.js
+++ src/js/fullscreen.js
@@ -252,6 +252,13 @@ class Fullscreen {
 
     // iOS native fullscreen doesn't need the request step
     if (browser.isIos && this.player.config.fullscreen.iosNative) {
+      if (this.player.provider === 'vimeo') {
+        this.player.requestFullscreen().then(function () {
+          console.log('entered')
+        }).catch(function (error) {
+          console.log('error occurred:', error)
+        })
+      }
       this.target.webkitEnterFullscreen();
     } else if (!Fullscreen.native || this.forceFallback) {
       this.toggleFallback(true);
